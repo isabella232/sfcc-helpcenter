844f70580ba60982f6f3a02cac7adbfe2b414940
NolwennP <nolwenn.poirier@akeneo.com>
2019-03-28 18:04:52 +0100

    init with the basics

---
+++ content/md/article-example.md
@@ -0,0 +1,7 @@
+---
+id: example-article
+themes: theme1
+title: This is an example article
+---
+
+# Should be removed once initiated with true articles
---
+++ tasks/copy-assets.js
@@ -0,0 +1,42 @@
+/**
+ * Copy html, css, js and image files into dist directory
+ */
+var gulp = require('gulp');
+var rename = require('gulp-rename');
+var merge = require('merge-stream');
+
+var majorVersion = 'v20';
+
+gulp.task('copy-assets', ['clean-dist'], function(){
+    var lib = gulp.src([
+            'node_modules/jquery/dist/jquery.min.js',
+            'node_modules/handlebars/handlebars.min.js',
+            'node_modules/bootstrap/dist/js/bootstrap.min.js',
+            'node_modules/docsearch.js/dist/cdn/docsearch.min.js'])
+        .pipe(gulp.dest('dist/sfcc/' + majorVersion + '/js/'));
+    var css = gulp.src(['node_modules/docsearch.js/dist/cdn/docsearch.min.css'])
+        .pipe(gulp.dest('dist/sfcc/' + majorVersion + '/css/'));
+    var fonts = gulp.src(['node_modules/typeface-comfortaa/files/*',
+              'node_modules/lato-font/fonts/lato-light/*',
+              'node_modules/lato-font/fonts/lato-light-italic/*',
+              'node_modules/lato-font/fonts/lato-normal/*',
+              'node_modules/lato-font/fonts/lato-normal-italic/*',
+              'node_modules/lato-font/fonts/lato-medium/*',
+              'node_modules/lato-font/fonts/lato-medium-italic/*',
+              'node_modules/lato-font/fonts/lato-semibold/*',
+              'node_modules/lato-font/fonts/lato-semibold-italic/*',
+              'node_modules/lato-font/fonts/lato-bold/*',
+              'node_modules/lato-font/fonts/lato-bold-italic/*'])
+        .pipe(gulp.dest('dist/sfcc/' + majorVersion + '/fonts/'));
+    var img = gulp.src('content/img/**/*')
+        .pipe(rename({dirname: ''}))
+        .pipe(gulp.dest('dist/sfcc/' + majorVersion + '/img/'));
+    var img = gulp.src('content/pdf-user-guides/**/*')
+        .pipe(rename({dirname: ''}))
+        .pipe(gulp.dest('dist/sfcc/' + majorVersion + '/pdf-user-guides/'));
+    var articleImg = gulp.src('content/md/*/img/*')
+        .pipe(rename({dirname: ''}))
+        .pipe(gulp.dest('dist/sfcc/' + majorVersion + '/img/'));
+
+    return merge(lib, css, fonts, img, articleImg);
+});
